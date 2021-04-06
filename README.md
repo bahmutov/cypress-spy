481f397a14d4778571ed69a431504ab78e54d1a1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-06 12:31:25 -0400

    test fails

---
+++ README.md
@@ -0,0 +1,5 @@
+- `npm install`
+- `npm start` in one terminal
+- `npx cypress open` from another terminal
+- click on the `gtag.js`
+
--- cypress/integration/gtag.js
+++ cypress/integration/gtag.js
@@ -1,3 +1,4 @@
+/// <reference types="cypress" />
 describe('Google gtag events', () => {
 
     // Cypress.on('window:before:load', (win) => {
@@ -7,14 +8,14 @@ describe('Google gtag events', () => {
     //     win.gtag = cy.spy().as('gtag')
     // })
 
-    Cypress.on('window:before:load', (win) => {
-        // because this is called before any scripts
-        // have loaded - the ga function is undefined
-        // so we need to create it.
-        win.gtag = cy.stub().as('gtag')
-        // cy.spy(win, '@gtag-stub').as('gtag')
-        // cy.spy(win, 'gtag').as('gtag')
-    })
+    // Cypress.on('window:before:load', (win) => {
+    //     // because this is called before any scripts
+    //     // have loaded - the ga function is undefined
+    //     // so we need to create it.
+    //     win.gtag = cy.stub().as('gtag')
+    //     // cy.spy(win, '@gtag-stub').as('gtag')
+    //     // cy.spy(win, 'gtag').as('gtag')
+    // })
 
     // beforeEach(function () {
     //     cy.intercept('www.googletagmanager.com', { statusCode: 503 })
@@ -28,13 +29,20 @@ describe('Google gtag events', () => {
             .should('exist')
     })
 
-    it('visit index page 2', () => {
+    it('has gtag', () => {
         cy.visit('/index.html')
-        // cy.visit('/index.html', {
-        //     onBeforeLoad(win) {
-        //         cy.spy(win, 'gtag').as('gtag')
-        //     },
-        // })
+            .then(win => {
+            expect(win).to.have.property('gtag')
+        })
+    })
+
+    it('visit index page 2', () => {
+        // cy.visit('/index.html')
+        cy.visit('/index.html', {
+            onBeforeLoad(win) {
+                // cy.spy(win, 'gtag').as('gtag')
+            },
+        })
         cy.get('@gtag').should('be.called')
 
     })
@@ -61,18 +69,18 @@ describe('Google gtag events', () => {
         cy.get('@gtag').should('be.called', 'event', 'view_item')
     })
 
-    it('visit index page 4', () => {
-
+    it.only('visit index page 4', () => {
         const gtag = cy.stub().as('gtag')
         cy.on('window:before:load', (win) => {
+            console.log('win before load')
             Object.defineProperty(win, 'gtag', {
                 // configurable: false,
                 get: () => gtag,
                 set: () => { },
             })
-            cy.visit('/index.html')
         })
-        
+        cy.visit('/index.html')
+
         cy.get('@gtag').should('be.called')
         cy.get('@gtag').should('be.called', 'event', 'view_item')
     })
